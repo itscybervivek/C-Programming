e8832589a5154d8cf5a853ab81ecd8c0a0d2a117
itscybervivek <er.vivek.social@gmail.com>
2025-01-02 21:37:37 +0545

    data types in c

---
+++ Chapter 1/03_data_types.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    // int a;
+    // a = 2;
+    int a = 2;
+
+    // float b = 1.4;
+    float b;
+    b = 1.4;
+
+    // char c = 'x';
+    char c;
+    c = 'x';
+printf("the value of a is %d", a);
+    printf("the value of b is %f", b); 
+    printf("the value of c is %c", c); 
+
+    printf("the value of a is %d\n", a); // \n is used to oputput code in seperate line
+    printf("the value of b is %f\n", b); 
+    printf("the value of c is %c\n", c); 
+    return 0;
+}
