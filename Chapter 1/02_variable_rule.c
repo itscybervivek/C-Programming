2ed5e4e51749429bcebfe7cd7d53ba8fb762577b
itscybervivek <er.vivek.social@gmail.com>
2025-01-02 21:36:46 +0545

    variable rule

---
+++ Chapter 1/02_variable_rule.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a; //allowed
+    int b; //allowed
+    int harry; //allowed
+    int harry_good; //allowed
+    int harry is good;  //not allowed
+    int $; //not allowed
+    return 0;
+}
