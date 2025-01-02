094635720324e77912da8a9dc7301290af8033b4
itscybervivek <er.vivek.social@gmail.com>
2025-01-02 21:39:32 +0545

    comments in c

---
+++ Chapter 1/05_comments.c
@@ -0,0 +1,9 @@
+// This is a single line comment.
+
+/*this is a multi line comment
+this is a multi line comment */
+#include<stdio.h>
+int main(){
+    printf("Hellow world"); // this command print Hellow world
+    return 0; // return 0 returns 0 to the OS
+}
