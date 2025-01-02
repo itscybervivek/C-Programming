b37e399459775d5faa748ab216612ab97c54e3da
itscybervivek <er.vivek.social@gmail.com>
2025-01-02 21:41:34 +0545

    escape sequence

---
+++ Chapter 1/07_escape_sequence.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    char ch = 'a';
+    ch = 'b';
+    ch = '\n'; // represents new line
+    printf("Hey I am good \n nice");
+    printf("Hey I am good \t nice"); // /t gives many space like Tab
+    printf("Hey I am good \t nice"); // /t gives many space like Tab
+    printf("Hey I am good \" nice"); // /" helps to print "
+    printf("Hey I am good \\ nice"); // /" helps to print backslash
+    
+    return 0;
+}
