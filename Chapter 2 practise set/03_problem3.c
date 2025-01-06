73f17ed54f05cee0da5da755016dcf66bd0697d2
itscybervivek <er.vivek.social@gmail.com>
2025-01-06 17:19:44 +0545

    problem 3

---
+++ Chapter 2 practise set/03_problem3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    // Write  a program to check weather a number is divisible by 97 or not.
+    int a = 460;
+    printf("%d", a%97);
+    return 0;
+    //if it is divisivle then the answer will me 0
+}
