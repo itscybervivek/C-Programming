c088603cfd1a62426434cc47773811ac86b3a201
itscybervivek <er.vivek.social@gmail.com>
2025-01-06 17:20:52 +0545

    problem 5

---
+++ Chapter 2 practise set/05_problem5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // 3.0 + 1 will be:
+    // Integer
+    // floating point number
+    // Character
+    // correct answer is float
+    float a = 3.0 + 1;
+    printf("The value of a is %f", a);
+    return 0;
+}
