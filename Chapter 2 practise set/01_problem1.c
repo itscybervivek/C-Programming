9074d8bae7151b16c937c32d72dec89e0186500a
itscybervivek <er.vivek.social@gmail.com>
2025-01-06 17:18:30 +0545

    problem 1

---
+++ Chapter 2 practise set/01_problem1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    // Which of the following is invalid ic C?
+    int a = 1;
+    int b = a; // this is true
+    int v = 3*3; // this is true
+    // char dt = '21 dec 2020'; // this is wrong 
+    // printf("%d and %d and %c", b, v, dt)
+    return 0;
+}
