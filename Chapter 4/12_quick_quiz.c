9f758245bb1bbbbdcba50b839aaed85fa4fa2957
itscybervivek <er.vivek.social@gmail.com>
2025-01-10 09:44:50 +0545

    quick quiz

---
+++ Chapter 4/12_quick_quiz.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    // write a program to print n natural number
+    // in reverse order 
+
+    int n = 12;
+    for (int i = n; i ; i--){
+        printf("%d\n", i);
+    }
+    return 0;
+}
