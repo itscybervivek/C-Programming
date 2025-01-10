7f99c2c0841bdacd30cde132f147ff8313b300d0
itscybervivek <er.vivek.social@gmail.com>
2025-01-10 09:37:49 +0545

    increment and decrement operator

---
+++ Chapter 4/06_increment_decrement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int i = 5;
+    printf("The value of i is %d\n", i);
+    i = i + 5 ; //10
+    printf("The value of i is %d\n", i);
+    printf("The value of i is %d\n", i++); //10
+    printf("The value of i is %d\n",i); //11
+
+    // i++ prints i first and then increament i(Post increment Operator)
+    // ++i increments i first and then prints i(Post increment Operator)
+    i +=2; // Same as i= i+2
+    printf("The value of i is %d\n",i); // 13
+    return 0;
+}
