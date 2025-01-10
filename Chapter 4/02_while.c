23061f776ca638098033a25156f2dcc3f593721c
itscybervivek <er.vivek.social@gmail.com>
2025-01-10 09:34:09 +0545

    while loop

---
+++ Chapter 4/02_while.c
@@ -0,0 +1,18 @@
+// while (condition is true){
+//  code
+//   The block keeps executing as long as the condition is true.
+//  }
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i < 4)
+    {
+        printf("Happy Birthday!\n");
+        i = i + 1;
+    }
+
+    return 0;
+}
