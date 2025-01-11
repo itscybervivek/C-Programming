6f183bc06457cb1860a909d9876bf5c64e1f90ce
itscybervivek <er.vivek.social@gmail.com>
2025-01-11 09:21:24 +0545

    problem 3

---
+++ Chapter 4 practise set/03_problem3.c
@@ -0,0 +1,17 @@
+/*A do while loop is executed:
+1. at least once
+2. at least twice
+3. at most once*/
+
+// answer is at least once:
+
+// #include<stdio.h>
+
+// int main(){
+//     do
+//     {
+//         //code
+//     } while (/* condition */);
+    
+//     return 0;
+// }
