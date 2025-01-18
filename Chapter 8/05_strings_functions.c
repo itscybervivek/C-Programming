b2163ba4c94f66b575c300e97c8d4e413783e5e9
itscybervivek <er.vivek.social@gmail.com>
2025-01-18 14:37:32 +0545

    string function

---
+++ Chapter 8/05_strings_functions.c
@@ -0,0 +1,12 @@
+   #include<stdio.h>
+   #include<string.h>
+
+   int main(){
+    char st[] = "Harry";
+    printf("%d", strlen(st));
+    return 0;
+    //Above code prints the number of strings.
+
+    
+
+   }
