323c472a66bded9aa0796e3e9f37b2451050540d
itscybervivek <er.vivek.social@gmail.com>
2025-01-25 02:43:33 +0545

    dynamic memory allocation in c

---
+++ Chapter 11/01_dynamic_memory_allocation.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *ptr;
+    scanf("%d", &n);
+    // int arr[n]; // Not allowed in c
+    ptr = (int*) malloc(n * sizeof(int));
+    ptr[0] = 3;
+    ptr[1] = 6;
+    printf("%d", ptr[0]);
+    return 0;
+}
